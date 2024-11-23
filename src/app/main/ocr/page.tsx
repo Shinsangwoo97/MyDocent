99798773470bae6d43fda417084057bd58d3ffc2
shinsangwoo <ssw2570@naver.com>
2024-11-24 00:20:56 +0900

    feat: OCR 테스트완료 11/24 추후 백으로 요청구현해야함!

--- src/app/main/ocr/page.tsx
+++ src/app/main/ocr/page.tsx
@@ -1,11 +1,25 @@
-'use client'
+'use client';
 
 import React, { useState } from "react";
+
+type Field = {
+  valueType: string;
+  boundingPoly: object;
+  inferText: string;
+  inferConfidence: number;
+};
+
+type OCRResponse = {
+  images: {
+    fields: Field[];
+  }[];
+};
 
 export default function OCRRequest() {
   const APIGW_INVOKE_URL = process.env.NEXT_PUBLIC_APIGW_INVOKE_URL;
   const SECRET_KEY = process.env.NEXT_PUBLIC_OCR_SECRET_KEY;
   const [imageData, setImageData] = useState<string | null>(null);
+  const [inferText, setInferText] = useState<string | null>(null);
 
   // Base64 데이터를 사용하는 요청
   const requestWithBase64 = async () => {
@@ -16,7 +30,6 @@ export default function OCRRequest() {
 
     // Data URI에서 Base64 부분만 추출
     const base64String = imageData.split(",")[1]; // "data:image/jpeg;base64," 제거
-    console.log(base64String);
 
     try {
       const response = await fetch(`${APIGW_INVOKE_URL}`, {
@@ -28,10 +41,10 @@ export default function OCRRequest() {
         body: JSON.stringify({
           images: [
             {
-              format: "png", // 파일 형식 (예: jpeg, png)
-              name: "medium", // 이미지 이름
-              data: `"${base64String}"`, // 순수 Base64 문자열
-              url: null
+              format: "jpg", // 파일 형식 (예: jpeg, png)
+              name: "mediums", // 이미지 이름
+              data: `${base64String}`, // 순수 Base64 문자열
+              url: null,
             },
           ],
           lang: "ko",
@@ -43,8 +56,9 @@ export default function OCRRequest() {
       });
 
       if (response.ok) {
-        const data = await response.json();
-        console.log("Base64 요청 결과:", data);
+        const data: OCRResponse = await response.json();
+        const texts = data.images[0].fields.map((field) => field.inferText);
+        setInferText(texts.join(" ")); // inferText를 하나의 문자열로 저장
       } else {
         console.warn(
           "Base64 요청 중 오류:",
@@ -56,6 +70,7 @@ export default function OCRRequest() {
       console.warn("Base64 요청 중 오류:", error);
     }
   };
+  console.log("합쳐진 텍스트:", inferText);
 
   // 파일 업로드 시 이미지 로드 함수
   const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -84,12 +99,21 @@ export default function OCRRequest() {
 
       {/* 캡처된 이미지 표시 */}
       {imageData && <img src={imageData} alt="캡처된 이미지" />}
+      
       <button
         className="p-2 bg-blue-500 text-white rounded"
         onClick={requestWithBase64}
       >
         Base64 요청 보내기
       </button>
+
+      {/* 추출된 전체 텍스트 표시 */}
+      {inferText && (
+        <div className="mt-4">
+          <h3 className="font-bold">추출된 텍스트:</h3>
+          <p>{inferText}</p>
+        </div>
+      )}
     </div>
   );
 }
